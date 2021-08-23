c41604ad3c655534d2295d3cac048796f6f1e4f0
elena_kau <lelickau@yandex.ru>
2021-08-23 17:12:50 +0200

    node js express часть 2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const homeRoutes = require('./routes/home');
+const cardRoutes = require('./routes/card');
 const coursesRoutes = require('./routes/courses');
 const addRoutes = require('./routes/add');
 
@@ -15,11 +17,12 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/add', addRoutes);
+app.use('/card', cardRoutes);
 
 
 
