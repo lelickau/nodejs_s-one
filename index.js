4c5293cad325ff40fe6d223af319a8fcf47978d2
elena_kau <lelickau@yandex.ru>
2021-08-25 17:25:24 +0200

    mongoDB часть 2

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
 const coursesRoutes = require('./routes/courses');
+const ordersRoutes = require('./routes/orders');
 const addRoutes = require('./routes/add');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,12 +20,23 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('61262864948f9e00c011acd5');
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+})
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/add', addRoutes);
 app.use('/card', cardRoutes);
+app.use('/orders', ordersRoutes);
 
 const PORT = process.env.PORT || 3050;
 
@@ -33,7 +46,19 @@ async function start() {
       await mongoose.connect(url, {
         useNewUrlParser: true,
         useFindAndModify: false
-      })
+      });
+      const candidate = await User.findOne();
+      if(!candidate) {
+        const user = new User({
+          email: 'lelickau@yandex.ru',
+          name: 'Elena',
+          cart: {
+            items: []
+          },
+        });
+        await user.save();
+      }
+
       app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`)
       })
