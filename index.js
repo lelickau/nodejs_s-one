f38963de7bd0a17872ff1858328acb3e7bca2968
elena_kau <lelickau@yandex.ru>
2021-09-01 17:54:41 +0200

    валидация и файлы (express-validator)

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ const coursesRoutes = require('./routes/courses');
 const ordersRoutes = require('./routes/orders');
 const addRoutes = require('./routes/add');
 const authRoutes = require('./routes/auth');
+const profileRoutes = require('./routes/profile');
 const varMiddleware = require('./middleware/variables');
 const userMiddleware = require('./middleware/user');
+const errorHandler = require('./middleware/error');
+const fileMiddleware = require('./middleware/file');
 const keys = require('./keys');
 
 const app = express();
@@ -35,6 +38,7 @@ app.set('views', 'views');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.urlencoded({extended: true}));
 app.use(session({
   secret: keys.SESSION_SECRET,
@@ -42,6 +46,7 @@ app.use(session({
   saveUninitialized: false,
   store
 }));
+app.use(fileMiddleware.single('avatar'));
 app.use(csrf());
 app.use(flash());
 app.use(varMiddleware);
@@ -54,6 +59,9 @@ app.use('/add', addRoutes);
 app.use('/card', cardRoutes);
 app.use('/orders', ordersRoutes);
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3050;
 
